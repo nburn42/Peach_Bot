dd31f89466a2e5f5b3a1a37b0dcf34224f60f639
Nathan Burnham <nburn42@gmail.com>
2016-09-24 01:53:46 +0000

    fixed encoders

--- catkin_ws/src/PeachBot_2dnav/src/peach_odom.py
+++ catkin_ws/src/PeachBot_2dnav/src/peach_odom.py
@@ -1,64 +1,53 @@
-#include <ros/ros.h>
-#include <tf/transform_broadcaster.h>
-#include <nav_msgs/Odometry.h>
+#!/usr/bin/python
 
 import rospy
 import datetime
 from nav_msgs.msg import Odometry
-from geometry_msgs.msg import Twist
-from geometry_msgs.msg import Quaternion
-from geomerty_msgs.msg import TransformStamped
+from geometry_msgs.msg import Twist, Point, Quaternion, TransformStamped
 
 import tf
 
+import math
+
 def handle_odom(data):
-    odom_pub = rospy.Publisher("odom", Odometry)
+    global x
+    global y
+    global th
+    global br
+    global odom_pub
+    global last_time
 
-    br = tf.TransformBroadcaster()
-
-    last_time = ros.Time.now();
+    current_time = rospy.Time.now();
 
     vx = data.linear.x
     vy = data.linear.y
     vth = data.angular.z
 
     #compute odometry in a typical way given the velocities of the robot
-    double dt = (current_time - last_time).to_sec();
-    double delta_x = (vx * cos(th) - vy * sin(th)) * dt;
-    double delta_y = (vx * sin(th) + vy * cos(th)) * dt;
-    double delta_th = vth * dt;
+    dt = (current_time - last_time).to_sec() * 1000000;
+    delta_x = (vx * math.cos(th) - vy * math.sin(th)) * dt;
+    delta_y = (vx * math.sin(th) + vy * math.cos(th)) * dt;
+    delta_th = vth * dt;
 
     x += delta_x;
     y += delta_y;
     th += delta_th;
 
     #since all odometry is 6DOF we'll need a quaternion created from yaw
-    odom_quat = tf.createQuaternionMsgFromYaw(th);
-
-    #first, we'll publish the transform over tf
-    odom_trans = TransformStamped();
-    odom_trans.header.stamp = current_time;
-    odom_trans.header.frame_id = "odom";
-    odom_trans.child_frame_id = "base_link";
-
-    odom_trans.transform.translation.x = x;
-    odom_trans.transform.translation.y = y;
-    odom_trans.transform.translation.z = 0.0;
-    odom_trans.transform.rotation = odom_quat;
+    odom_quat = tf.transformations.quaternion_about_axis(th, (0,0,1));
 
     #send the transform
-    br.sendTransform(odom_trans);
+    br.sendTransform((x,y,0), odom_quat, current_time, "odom", "base_link");
 
     #next, we'll publish the odometry message over ROS
-    nav_msgs::Odometry odom;
+    odom = Odometry()
     odom.header.stamp = current_time;
     odom.header.frame_id = "odom";
 
     #set the position
-    odom.pose.pose.position.x = x;
-    odom.pose.pose.position.y = y;
-    odom.pose.pose.position.z = 0.0;
-    odom.pose.pose.orientation = odom_quat;
+    odom.pose.pose.position = Point(x,y,0.0);
+    odom.pose.pose.orientation = Quaternion(odom_quat[0], odom_quat[1], odom_quat[2], odom_quat[3]);
+    
 
     #set the velocity
     odom.child_frame_id = "base_link";
@@ -67,16 +56,21 @@ def handle_odom(data):
     odom.twist.twist.angular.z = vth;
 
     #publish the message
-    odom_pub.publish(odom);
+    #odom_pub.publish(odom.pose.pose.position, odom.pose.pose.orientation,
+    #    odom.header.stamp, odom.header.frame_id, odom.child_frame_id);
+
+    odom_pub.publish(odom)
 
     last_time = current_time;
 
 if __name__ == '__main__':
-    rospy.init("odometry_publisher", anonymous=True)
-    current_time = rospy.Time.now();
-    double x = 0.0;
-    double y = 0.0;
-    double th = 0.0;
+    rospy.init_node("odometry_publisher", anonymous=True)
+    last_time = rospy.Time.now();
+    x = 0.0
+    y = 0.0
+    th = 0.0
+    odom_pub = rospy.Publisher("odom", Odometry)
+    br = tf.TransformBroadcaster()
     rospy.Subscriber('/odom_raw',
                      Twist,
                      handle_odom)
